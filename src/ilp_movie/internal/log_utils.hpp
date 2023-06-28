7b68bc810c20f0ea7812bbed3dc531b0507ac38f
Tommy Hinks <tommy.hinks@ilpvfx.com>
2023-06-28 13:18:39 +0000

    Log functions for packets.

--- src/ilp_movie/internal/log_utils.hpp
+++ src/ilp_movie/internal/log_utils.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
-#include <ilp_movie/ilp_movie_export.hpp>
+#include <ilp_movie/ilp_movie_export.hpp>// ILP_MOVIE_NO_EXPORT
+#include <ilp_movie/log.hpp>// ilp_movie::LogMsg, ilp_movie::LogLevel
+
+struct AVFormatContext;
+struct AVPacket;
 
 namespace log_utils_internal {
 
@@ -16,5 +20,10 @@ namespace log_utils_internal {
 // one is automatically added to the final message that is built internally.
 ILP_MOVIE_NO_EXPORT
 void LogAvError(const char *msg, const int errnum = 0) noexcept;
+
+
+ILP_MOVIE_NO_EXPORT void LogPacket(const AVFormatContext *fmt_ctx,
+  const AVPacket *pkt,
+  const int log_level = ilp_movie::LogLevel::kTrace) noexcept;
 
 }// namespace log_utils_internal
